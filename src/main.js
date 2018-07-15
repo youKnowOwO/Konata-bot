55c026022580c625bea1ed43d202674ece5e2497
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-07-15 14:29:24 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-//Joke Stuff :P
+const { Client } = require('discord.js');
+const { readdir } = require('fs');
+const Modules = require('./handle/modules.js');
+
+class KanaSou extends Client {
+	constructor(ops){
+		super(ops);
+		this.commands = Modules.commands;
+		this.aliases = Modules.aliases;
+		this.help = Modules.help;
+		this.env = process.env;
+	}
+}
+
+const client = new KanaSou({
+	fetchAllMembers: true,
+	disableEveryone: true
+});
+
+readdir('./events', (e, files) => {
+	if(err) console.error(e);
+	files.forEach(f => {
+		const eventFunc = require(`./events/${f}`);
+		const eventName = f.split('.')[0];
+		client.on(eventName, (...args) => eventFunct.run(client, ...args));
+	});
+});
+
+client.login(client.env.TOKEN);
